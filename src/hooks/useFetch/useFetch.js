f03d56d92077d1b40d232578aa794e1d8b7b3e54
Reisel Valle <reiselvalle@gmail.com>
2023-11-13 15:48:01 -0500

    refactor view

--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -5,17 +5,14 @@ export const base_url = `https://swapi.dev/api/`;
 export const useFetch = (url)=>{
     const [ data, setData ] = useState(null);
     const [ loading, setLoading ] = useState(false);
-
+    
     useEffect(()=>{
-        setLoading(true);    
+        setLoading(true);
         fetch(url)
-            .then((response)=>response.json())
-            .then((results)=>{
-                setData(results);
-                setLoading(false);
-            })
-        
-        },[url])
+            .then(res => res.json())
+            .then(result => setData(result))
+            .finally(()=>setLoading(false))
+    },[url]);
     
     return {data, loading};
 }
--- src/pages/people/People.jsx
+++ src/pages/people/People.jsx
@@ -1,20 +1,16 @@
 import { base_url, useFetch } from "../../hooks/useFetch/useFetch";
 
 export default function People() {
-    const { data, loading} = useFetch(`${base_url}people`);
-
-    return (
-      <div>
-        {
-          loading ? <h3 style={{color: "#fff"}}> Loading ... </h3>
-                  : data && <ul>
-                    {
-                      data.results.map((people,i)=>{
-                        <li key={i} style={{ color: "#fff"}}> { people.name } </li>
-                      })
-                    }
-                  </ul>
-        }        
-      </div>
-    )
+  const { data, loading} = useFetch(`${base_url}people`);
+  console.log('data >> ', data);
+  return (
+    <div>
+      { loading 
+        ? <h3 style={{color: "#fff"}}> Loading ... </h3> 
+        : <ul>        
+            { data?.results?.map((people,i)=> (<li key={i}> { people.name } </li>)) } 
+          </ul>         
+      }
+    </div>
+  )
 }
