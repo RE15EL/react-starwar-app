dcdb37f0150ced379f6e9ec1d4827c72f9804415
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 12:02:43 -0500

    use-debounce fetching

--- src/components/paginator/Paginator.jsx
+++ src/components/paginator/Paginator.jsx
@@ -1,8 +1,11 @@
+import './Paginator.css';
 import { useEffect, useState } from "react";
 import { base_url } from "../../hooks/useFetch/useFetch";
+import { useDebounce } from 'use-debounce';
 
 export function Paginator({ data, setData, setLoading }) {
     const [ pageIndex, setPageIndex] = useState(1);
+    const [ debouncedPageIndex ]  = useDebounce(pageIndex, 400);
     const pagesTotal = data ? Math.trunc(data?.count/10): 1;
 
     const handlePaginator = (index)=>{
@@ -12,17 +15,17 @@ export function Paginator({ data, setData, setLoading }) {
 
     useEffect(()=>{
         setLoading(true);
-        fetch(`${base_url}people/?page=${pageIndex}`)
+        fetch(`${base_url}people/?page=${debouncedPageIndex}`)
           .then(res => res.json())
           .then(result => setData(result))
           .finally(()=> setLoading(false))
-      },[pageIndex])
+      },[debouncedPageIndex])
 
     return (
         <div className='paginator'>
-            <button className='btn-md' onClick={()=> handlePaginator(pageIndex-1)}>{"<"}</button>
+            <button disabled={pageIndex < 2} className='btn-md' onClick={()=> handlePaginator(pageIndex-1)}>{"<"}</button>
             <span> page {pageIndex} of {pagesTotal} </span>
-            <button className='btn-md' onClick={()=> handlePaginator(pageIndex+1)}>{">"}</button>
+            <button disabled={pageIndex === pagesTotal} className='btn-md' onClick={()=> handlePaginator(pageIndex+1)}>{">"}</button>
         </div>
     )
 }
